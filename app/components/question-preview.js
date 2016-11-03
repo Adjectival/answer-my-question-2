98192aaa64313d81746f90d7764b43e25a40cf26
Alexander Jacks <alexd.jacks@gmail.com>
2016-11-03 16:10:05 -0700

    adds buttons to hide/display notes on question-preview

--- app/components/question-preview.js
+++ app/components/question-preview.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  notesHiding: true,
+  actions: {
+    unhideNotes() {
+      this.set('notesHiding', false);
+    },
+    hideNotes() {
+      this.set('notesHiding', true);
+    },
+  }
 });
