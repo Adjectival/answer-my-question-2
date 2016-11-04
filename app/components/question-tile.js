0d83b99a852cfc110fd7192c2f52e6db2657528c
Alexander Jacks <alexd.jacks@gmail.com>
2016-11-04 13:22:08 -0700

    adds computed prop that lists number of answers per question on index

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   notesHide: true,
+  answerCount: Ember.computed('question', function() {
+    return this.get('question').get('answers').get('length');
+  }),
   actions: {
     unhideNotes() {
       this.set('notesHide', false);
