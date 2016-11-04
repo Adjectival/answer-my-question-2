ebd54b29ad5e27acac4058dff158f330def7c7ae
Alexander Jacks <alexd.jacks@gmail.com>
2016-11-04 11:43:25 -0700

    adds safety function to delete answer models before deleting a question

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     delete(question) {
-      if (confirm('Art thou sure?')) {
+      if (confirm('Remove this Question?')) {
         this.sendAction('destroyQuestion', question);
       }
     },
