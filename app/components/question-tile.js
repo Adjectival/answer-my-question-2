118aef5cdf56983dc4f74f8c9e19fb030de6e2af
Alexander Jacks <alexd.jacks@gmail.com>
2016-11-05 14:25:18 -0700

    adds service to display favorite'd questions; improved styling everywhere

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -5,12 +5,17 @@ export default Ember.Component.extend({
   answerCount: Ember.computed('question', function() {
     return this.get('question').get('answers').get('length');
   }),
+  favoriteQuestions: Ember.inject.service(),
   actions: {
     unhideNotes() {
       this.set('notesHide', false);
     },
     hideNotes() {
       this.set('notesHide', true);
+    },
+    addToFaves(question) {
+      this.get('favoriteQuestions').add(question);
+      console.log("Q tile emit");
     }
   }
 });
--- app/services/favorite-questions.js
+++ app/services/favorite-questions.js
@@ -1,19 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
-  favorites: [],
-  add(favorite){
-    if (this.get("topThreeGot")){
-      alert("You already selected your top 3 questions");
-    } else {
-      this.get('favorites').pushObject(favorite);
-    }
-  },
-  remove(favorite){
-    this.get('favorites').removeObject(favorite);
-  },
-  includes(favorite){
-    return this.get('favorites').includes(favorite);
-  },
-  topThreeGot: Ember.computed.get('favorites.length', 3)
+  questions: [],
+
+  add(question) {
+    this.get('questions').pushObject(question);
+  }
 });
